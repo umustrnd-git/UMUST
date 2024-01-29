2dd9836bdbb1385b8100789fd9bacd4821a59520
degull <skytmdgus99@gmail.com>
2024-01-29 17:29:11 +0900

    앨범 레이아웃 구현

--- src/routes/List/Album.js
+++ src/routes/List/Album.js
@@ -1,11 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import * as S from './Album.styled';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+
 
 const Album = () => {
+
+   const [posts, setPosts] = useState([]);
+
+/*    useEffect(() => {
+      fetch('yourApiEndpoint')
+         .then(response => response.json())
+         .then(data => setPosts(data))
+         .catch(error => console.error('Error fetching posts:', error));
+   }, []);
+ */
+
+   // 임시데이터 -> 지울거
+   const samplePosts = [
+      { id: 1, thumbnailUrl: '/img/Post1.jpg', title: 'Post 1' },
+      { id: 2, thumbnailUrl: '/img/Post2.jpg', title: 'Post 2' },
+      { id: 3, thumbnailUrl: '/img/Post3.jpg', title: 'Post 3' },
+      { id: 4, thumbnailUrl: '/img/Post3.jpg', title: 'Post 4' },
+      { id: 4, thumbnailUrl: '/img/Post3.jpg', title: 'Post 4' },
+      { id: 4, thumbnailUrl: '/img/Post3.jpg', title: 'Post 4' },
+      { id: 4, thumbnailUrl: '/img/Post3.jpg', title: 'Post 4' },
+      { id: 4, thumbnailUrl: '/img/Post3.jpg', title: 'Post 4' },
+   ];
+
    return (
-      <div>
-         앨범
-      </div>
+      <S.Wrapper>
+         <Header />
+         <S.WrapImage src="/img/NoticeLogo.png" />
+         <S.MenuTab>구현예정</S.MenuTab>
+
+         <S.MainContainer>
+            <S.Title>앨범</S.Title>
+            <S.Memo>유머스트알엔디의 일상을 기록합니다.</S.Memo>
+
+
+{/* 샘플 데이터 -> 지울거 */}
+         <S.AlbumWrapper>
+            {samplePosts.map(post => (
+               <S.PostItem key={post.id}>
+                  <S.Thumbnail src={post.thumbnailUrl} alt={`Thumbnail for ${post.title}`} />
+                  <S.PostTitle>{post.title}</S.PostTitle>
+               </S.PostItem>
+            ))}
+         </S.AlbumWrapper>
+
+
+{/* 원본 */}
+{/* 
+         <S.AlbumWrapper>
+             {posts.map(post => (
+               <S.PostItem key={post.id}>
+                  <S.Thumbnail src={post.thumbnailUrl} alt={`Thumbnail for ${post.title}`} />
+                  <S.PostTitle>{post.title}</S.PostTitle>
+               </S.PostItem>
+            ))}
+            </S.AlbumWrapper>
+
+             */}
+         </S.MainContainer>
+
+         <Footer />
+      </S.Wrapper>
+
+
    );
 };
 
 export default Album;
+
--- src/routes/List/Album.styled.js
+++ src/routes/List/Album.styled.js
@@ -1,1 +1,66 @@
 import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+   width: 100%;
+   background: #fff;
+`;
+
+export const WrapImage = styled.img`
+   width: 100%;
+   height: 230px;
+`;
+
+export const MenuTab = styled.div`
+   margin: 0 auto;
+   width: 100%;
+   height: 50px;
+   background: #9c9;
+`;
+
+export const MainContainer = styled.div`
+   max-width: 1280px;
+   margin: 0 auto;
+   width: 100%;
+   height: 500px;
+`;
+
+export const Title = styled.div`
+   font-size: 17px;
+   font-weight: bold;
+   padding: 30px 30px 15px 30px;
+`;
+
+export const Memo = styled.div`
+   font-size: 14px;
+   padding: 0 30px 30px 30px;
+`;
+
+export const AlbumWrapper = styled.div`
+   display: grid;
+   grid-template-columns: repeat(4, 1fr);
+   gap: 20px;
+   padding: 20px;
+`;
+
+export const PostItem = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   text-align: center;
+   background: #f5f5f5;
+   border: 1px solid #ddd;
+   padding: 10px;
+`;
+
+export const Thumbnail = styled.img`
+   width: 100%;
+   height: auto;
+   max-height: 150px; /* Set a maximum height for the thumbnail */
+   margin-bottom: 10px;
+`;
+
+export const PostTitle = styled.div`
+   font-size: 14px;
+   font-weight: bold;
+`;
+
